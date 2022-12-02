0e8c36f464e5a747ac4fbe21ec9e5cfafbc8fad7
Mark Lhester Agellon <kianirel56@gmail.com>
2022-12-03 00:55:52 +0800

    Add project tab

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const body = document.querySelector("body"),
       sidebar = body.querySelector(".sidebar"),
       toggle = body.querySelector(".toggle");
-      
+
 
       toggle.addEventListener("click", () =>{
         sidebar.classList.toggle("close");      
       });
+
+const new_question = document.querySelector("#new_question");
+const add_question = document.querySelector(".add-btn");
+const list = document.querySelector(".list-of-questions ul")
+add_question.addEventListener("click", (e) =>{
+  if(add_question.value != ""){
+    e.preventDefault;
+    const myLi = document.createElement("li");
+    myLi.innerHTML = new_question.value;
+    list.appendChild(myLi)
+  }
+})
